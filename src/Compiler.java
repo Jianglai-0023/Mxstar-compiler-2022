3daf7c62cad51b79d13d83830aae90d891839d40
Jianglai-0023 <90485159+Jianglai-0023@users.noreply.github.com>
2022-10-30 10:52:00 +0800

    10

--- src/Compiler.java
+++ src/Compiler.java
@@ -17,9 +17,9 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Compiler {
     public static void main(String[] args) throws Exception{
-        String name = "testcases/sema/basic-package/basic-32.mx";
-        InputStream input_stream = new FileInputStream(name);
-//        InputStream input_stream = System.in;
+//        String name = "testcases/sema/basic-package/basic-32.mx";
+//        InputStream input_stream = new FileInputStream(name);
+        InputStream input_stream = System.in;
         CharStream charstream = CharStreams.fromStream(input_stream);
         try
         {
